c282559091c60b50705ace69c1a78b23194b0f1c
Rylan Kettles <rkettles14@gmail.com>
2018-09-22 14:09:04 -0600

    Completed and tested Circle
    i think it works...

---
+++ circle.cpp
@@ -0,0 +1,58 @@
+//circle.cpp
+//Author: Rylan Kettles and Sebastion Schroh
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include "circle.h"
+#include "shape.h"
+#define PI 3.14159265358979323846
+using namespace std;
+
+Circle::Circle(double x, double y, char* name, double rad):Shape(x,y,name){
+	radius = rad;
+}
+
+Circle::Circle(const Circle& source): Shape(source), radius(source.radius){
+}
+
+Circle& Circle::operator =(const Circle& rhs){
+	Shape::operator = (rhs);
+	radius = rhs.radius;
+	return *this;
+
+}
+
+double Circle::getRadius() const{
+	return radius;
+}
+
+void Circle::setRadius(double rad){
+	radius = rad;
+}
+
+double Circle::area(){
+	double a = PI * pow(radius, 2);
+	return a;
+}
+
+double Circle::perimeter(){
+	return PI * radius * 2;
+}
+
+void Circle::display(){
+	Shape::display();
+	cout << "Radius: " << setprecision(2) << radius << endl;
+	cout << "Area: "<< setprecision(4) << area() << endl;
+	cout << "Perimeter: "<< setprecision(2) << perimeter()<<endl;
+}	
+//main function for testing
+/*
+int main(){
+	Circle c(12.9,13.8, "test_circle", 22);
+	
+	c.display();
+	
+	return 0;
+}
+*/
---
+++ circle.h
@@ -0,0 +1,40 @@
+//circle.h
+//Author: Rylan Kettles and Sebastion Schroh
+
+
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include "shape.h"
+
+
+using namespace std;
+
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+class Circle : public Shape{
+	
+	protected:
+		double radius;
+		
+	public:	
+
+		Circle(double x, double y, char* name, double rad);
+	
+		Circle(const Circle& source);
+		
+		Circle& operator =(const Circle& rhs);
+		
+		double getRadius() const;
+		
+		void setRadius(double rad);
+	
+		virtual double area();
+
+		virtual double perimeter();
+		
+		void display();
+
+};
+#endif
